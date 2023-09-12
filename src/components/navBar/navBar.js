a43bb18158c935d16d1584852b4efaf9e11cb783
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-12 14:14:13 -0600

    Finished making the navbar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 // CSS File
 import "./navBar.css";
 
 export default function NavBar() {
+    // Function for showing navbar links
+    const handleShowLinks = () => {
+        // set the variable for myTopnav
+        let x = document.getElementById("myTopnav");
+        // Begin conditional
+        if (x.className === "container topNav") {
+            x.className += " responsive";
+        } else {
+            x.className = "container topNav"
+        }
+    }
+
     return (
-        <div className="topNav">
+        <div className="container topNav" id="myTopnav">
+            <button className="icon" onClick={handleShowLinks}>
+                <GiHamburgerMenu />
+            </button>
             <a href="/">Home</a>
             <a href="/portfolio">Portfolio</a>
             <a href="/contact">Contact</a>
