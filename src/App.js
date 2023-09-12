7621fe240ea3ee16d9202f38086e635314ae5dab
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-12 12:57:05 -0600

    Setup routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from "react";
+// React router dom
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from 'react-router-dom'
 
  // Styling
 import './App.css'
 
+// Pages
+import HomePage from "./pages/home-page";
+import PortfolioPage from "./pages/portfolio-page";
+import ContactPage from "./pages/contact-page";
+
 function App() {
     return (
-        <div>The App is ready to go!</div>
+        <>
+            {/* Setup the routes */}
+            <BrowserRouter>
+                <Routes>
+                    {/* HomePage Route */}
+                    <Route path='/' element={<HomePage />} />
+                    {/* PortfolioPage Route */}
+                    <Route path='/portfolio' element={<PortfolioPage />} />
+                    {/* ContactPage Route */}
+                    <Route path='/contact' element={<ContactPage />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
