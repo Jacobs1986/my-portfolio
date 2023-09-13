572d99eac38c4a2eb68272d2ba9c00df6044d6da
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-13 17:08:31 -0600

    Got the menu and animation working the way that I want

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -4,12 +4,22 @@ import React from "react";
 import "./navBar.css";
 
 export default function NavBar() {
+    // Function that handles showing the menu
+    const handleToggleMenu = () => {
+        var x = document.getElementById("navigationBar");
+        if (x.className === "navigationBar") {
+            x.className += " responsive"
+        } else {
+            x.className = "navigationBar"
+        }
+    }
+
     return (
         <div>
             <div>
-                <button className="navigationToggle">Menu</button>
+                <button className="navigationToggle" onClick={handleToggleMenu}>Menu</button>
             </div>
-            <div className="navigationBar">
+            <div className="navigationBar" id="navigationBar">
                 <a href="/">Home</a>
                 <a href="/portfolio">Portfolio</a>
                 <a href="/contact">Contact</a>
