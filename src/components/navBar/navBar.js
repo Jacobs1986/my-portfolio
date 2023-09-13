c54177451cee9e239b298b44c2e8162ebe8ffda4
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-13 15:37:51 -0600

    I'm remaking the navBar, I didn't like the way it looked

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,31 +1,10 @@
 import React from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
 
 // CSS File
 import "./navBar.css";
 
 export default function NavBar() {
-    // Function for showing navbar links
-    const handleShowLinks = (event) => {
-        event.preventDefault();
-        // set the variable for myTopnav
-        let x = document.getElementById("myTopnav");
-        // Begin conditional
-        if (x.className === "container topNav") {
-            x.className += " responsive";
-        } else {
-            x.className = "container topNav"
-        }
-    }
-
     return (
-        <div className="container topNav" id="myTopnav">
-            <button className="icon" onClick={handleShowLinks}>
-                <GiHamburgerMenu />
-            </button>
-            <a href="/">Home</a>
-            <a href="/portfolio">Portfolio</a>
-            <a href="/contact">Contact</a>
-        </div>
+        <div>This is the navigation bar</div>
     );
 };
