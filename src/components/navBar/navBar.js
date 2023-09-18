c8e1a59968ec54c3c325f44058610fdb0d258791
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-18 12:34:34 -0600

    Finished putting in the link for the navBar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,10 +1,33 @@
 import React from "react";
+// Fa icon
+import { FaCaretDown } from "react-icons/fa";
 
 // CSS File
 import "./navBar.css";
 
 export default function NavBar() {
     return (
-        <div>This is the navBar</div>
+        <div>
+            {/* Navigation bar */}
+            <div className="topNav" id="myTopNav">
+                {/* Home page Link */}
+                <a href="/">Home</a>
+                {/* Portfolio Dropdown */}
+                <div className="dropdown">
+                    <button className="dropbtn">Portfolio
+                        <FaCaretDown />
+                    </button>
+                    {/* Dropdown content */}
+                    <div className="dropdown-content">
+                        {/* Featured link */}
+                        <a href="/featured">Featured</a>
+                        {/* Portfolio */}
+                        <a href="/portfolio">All Projects</a>
+                    </div>
+                </div>
+                {/* Contact */}
+                <a href="/contact">Contact</a>
+            </div>
+        </div>
     );
 };
